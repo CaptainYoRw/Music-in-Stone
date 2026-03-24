60785b4e8cad9d363d1d2813444b12b1a6e6d979
CaptainYoRw <47063784+CaptainYoRw@users.noreply.github.com>
2026-03-24 22:25:08 +0300

    Added tags for future columns and added large florentine arches

---
+++ src/main/java/com/mrclon_51/musicinstone/TagsRegistry.java
@@ -0,0 +1,22 @@
+package com.mrclon_51.musicinstone;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+import static net.minecraft.tags.TagEntry.tag;
+
+public class TagsRegistry
+{
+       public static class Blocks
+       {
+           public static final TagKey<Block> COLUMN_BASE = tag("column_base");
+               public static final TagKey<Block> COLUMN_CAPITAL = tag("column_capital");
+
+           private static TagKey<Block> tag(String name)
+           {
+               return TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("musicinstone", name));
+           }
+       }
+}
